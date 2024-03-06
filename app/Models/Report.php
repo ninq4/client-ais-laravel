ce51735ffc64a8f3c402892f3fec74f20a3cab51
ninq4 <nik.dolgov.05@mail.ru>
2024-03-06 19:31:10 +0300

    Создал модули

--- app/Models/Report.php
+++ app/Models/Report.php
@@ -8,4 +8,28 @@ use Illuminate\Database\Eloquent\Model;
 class Report extends Model
 {
     use HasFactory;
+
+    protected $table = 'reports';
+
+    protected $fillable = [
+        'title',
+        'description',
+        'client_id',
+        'status_id',
+        'executer_id'
+    ];
+
+
+    public function status()
+    {
+        $this->belongsTo(Status::class, 'status_id', 'id');
+    }
+    public function client()
+    {
+        $this->belongsTo(Client::class, 'client_id', 'id');
+    }
+    public function executer()
+    {
+        $this->belongsTo(Executer::class, 'executer_id', 'id');
+    }
 }
--- app/Models/Status.php
+++ app/Models/Status.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Status extends Model
 {
     use HasFactory;
+    protected $table = 'statuses';
+
+    protected $fillable = [
+        'status'
+    ];
 }
